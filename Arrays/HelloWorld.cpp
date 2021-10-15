330bb77bd3a37aa8e59051263fa4357b62a762ef
Sai Shubham <saishubhamray@gmail.com>
2021-10-15 12:19:04 +0530

    Hello World

---
+++ Arrays/HelloWorld.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int a = 2;
+    
+    int b = a * a;
+    cout << b;
+
+    system("pause"); //For Debugging, ignore it
+    return 0;
+}
