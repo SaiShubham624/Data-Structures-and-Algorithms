5f0892eb833435f97bac44ae9753456e1325cd54
Sai Shubham <saishubhamray@gmail.com>
2021-10-15 12:21:32 +0530

    Swap Two Numbers

---
+++ Arrays/Problem3_SwapTwoNumbers.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+/*
+Problem: You will be given two Numbers and you have to swap them
+*/
+
+int main()
+{
+    int a = 4;
+    int b = 5;
+    // Way 1: Create a third variable to swap the two positive integers    
+    // cout << "Before Swapping" << endl;
+    // cout << "a = " << a << endl;
+    // cout << "b = " << b << endl;
+    // int temp = a;
+    // a = b;
+    // b = temp;
+    // cout << "After Swapping" << endl;
+    // cout << "a = " << a << endl;
+    // cout << "b = " << b << endl;
+    // Way 2: Use the bitwise Operator
+    // a = a^b;
+    // b = a^b;
+    // a = a^b;
+    // cout << "After Swapping" << endl;
+    // cout << "a = " << a << endl;
+    // cout << "b = " << b << endl;
+    return 0;
+}
