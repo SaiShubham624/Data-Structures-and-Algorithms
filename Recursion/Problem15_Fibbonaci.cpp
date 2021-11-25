11bc6c4a713c7c2c19b39ca12de4e7e35a424241
Sai Shubham <saishubhamray@gmail.com>
2021-11-25 20:54:06 +0530

    Added the Iterative Approach

--- Recursion/Problem15_Fibbonaci.cpp
+++ Recursion/Problem15_Fibbonaci.cpp
@@ -6,6 +6,7 @@ Problem:
 You have to find the n(th) number in the Fibbonaci Series
 */
 
+// Recursive Approach
 int fibbonaci(int n){
     if(n <= 1){
         return 0;
@@ -16,14 +17,26 @@ int fibbonaci(int n){
     return fibbonaci(n-2) + fibbonaci(n-1);
 }
 
+// Iterative Approach
+int fib(int n){
+    int a = 0, b = 1, c, i;
+    if (n == 0){
+        return a;
+    }
+    for (i = 2; i <= n; i++)
+    {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
+
 int main(){
 
-    int n;
-    cout << "Enter the number: ";
-    cin >> n;
-    int ans = fibbonaci(n);
-    cout << "The Fibbonaci of " << n << " is " << ans << endl;
-
-    system("pause"); //For Debugging, ignore it
+    int n = 4;
+    int ans = fib(n);
+    cout << "Answer is " << ans << endl;
+    // system("pause"); //For Debugging, ignore it
     return 0;
 }
